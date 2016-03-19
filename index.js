da38ea888fde30825cac673f7d8d6cfaade4b99f
Roshan Piumal <ropiumal@gmail.com>
2016-03-19 19:24:58 +0530

    improved the api to support for multiple routes and route params

--- index.js
+++ index.js
@@ -3,12 +3,65 @@ var app = express();
 var Datastore = require('nedb');
 var db = {};
 
+var setupResponder = function (res) {
+    return function (err, results) {
+        if(err){
+            res.send(JSON.stringify(err));
+        }else {
+            res.send(JSON.stringify(results));
+        }
+    };
+};
+
+
 db.movies = new Datastore({filename: 'db/movies', autoload: true});
 
 app.use(express.bodyParser());
 
+//routes
 app.get('/',function (req, res) {
     res.end("The API is working");
+})
+.post('/movies', function (req, res) {
+    var body = req.body;
+    var respond = setupResponder(res);
+
+    res.set('Content-type', 'application/json');
+
+    switch (body.action) {
+        case "viewList":
+            db.movies.find({}, respond);
+            break;
+
+        case "addNew":
+            db.movies.insert({title: body.title}, respond);
+            break;
+
+        default:
+            respond("No action given");
+    }
+
+
+})
+.post('/movies/:id',function (req, res) {
+    var body = req.body;
+    var respond = setupResponder(res);
+
+    res.set('Content-type', 'application/json');
+
+    switch (body.action) {
+        case "rate":
+            db.movies.update({ _id: req.params.id}, { $set: {rating: body.rating}}, function (err, num) {
+                respond(err, {success: num +" records updated"});
+            });
+            break;
+        case "view":
+            db.movies.findOne({_id: req.params.id}, respond);
+            break;
+        default:
+            respond("No action given");
+    }
+
 })
 .post('/rpc', function (req, res) {
     var body = req.body;
@@ -21,7 +74,7 @@ app.get('/',function (req, res) {
         }
     }
 
-    res.set('Content-type', 'application/json')
+    res.set('Content-type', 'application/json');
 
     switch (body.action) {
         case "getMovies":
