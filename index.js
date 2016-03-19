6790c6a10118497750af5f77480e404444c48b53
Roshan Piumal <ropiumal@gmail.com>
2016-03-20 00:47:39 +0530

    modified the api to use http verbs and status codes

--- index.js
+++ index.js
@@ -2,98 +2,84 @@ var express = require('express');
 var app = express();
 var Datastore = require('nedb');
 var db = {};
+var responder = require('./httpResponder');
 
-var setupResponder = function (res) {
-    return function (err, results) {
-        if(err){
-            res.send(JSON.stringify(err));
-        }else {
-            res.send(JSON.stringify(results));
-        }
-    };
-};
-
+var port = process.argv[2] || 3000;
+var root = "http://localhost:" + port;
 
 db.movies = new Datastore({filename: 'db/movies', autoload: true});
 
+//Add an index (if we try to add a duplicate title this will throw an error)
+db.movies.ensureIndex({ fieldName: 'title', unique: true});
+
 app.use(express.bodyParser());
+app.use(function (req, res, next) {
+    res.type('application/json');
+    res.locals.respond = responder.setup(res);
+    next();
+});
 
-//routes
-app.get('/',function (req, res) {
-    res.end("The API is working");
-})
-.post('/movies', function (req, res) {
-    var body = req.body;
-    var respond = setupResponder(res);
+app.get('/movies', function (req, res) {
+    db.movies.find({}, res.locals.respond);
+});
 
-    res.set('Content-type', 'application/json');
-
-    switch (body.action) {
-        case "viewList":
-            db.movies.find({}, respond);
-            break;
-
-        case "addNew":
-            db.movies.insert({title: body.title}, respond);
-            break;
-
-        default:
-            respond("No action given");
+app.post('/movies', function (req, res) {
+    if(!req.body.title){
+        res.status(400);
+        res.json(400, {error: "A title is required tp create a movie"});
+        return;
     }
-
-
-})
-.post('/movies/:id',function (req, res) {
-    var body = req.body;
-    var respond = setupResponder(res);
-
-    res.set('Content-type', 'application/json');
-
-    switch (body.action) {
-        case "rate":
-            db.movies.update({ _id: req.params.id}, { $set: {rating: body.rating}}, function (err, num) {
-                respond(err, {success: num +" records updated"});
-            });
-            break;
-        case "view":
-            db.movies.findOne({_id: req.params.id}, respond);
-            break;
-        default:
-            respond("No action given");
-    }
-
-})
-.post('/rpc', function (req, res) {
-    var body = req.body;
-
-    var respond = function (err, results) {
+    db.movies.insert({ title: req.body.title }, function (err, created) {
         if(err){
-            res.send(JSON.stringify(err));
-        }else {
-            res.send(JSON.stringify(results));
+            res.json(500, err);
+            return;
         }
-    }
+        res.set('Location', root + '/movies/' + created._id);
+        res.json(201, created)
+    });
+});
 
-    res.set('Content-type', 'application/json');
+app.get('/movies/:id', function (req, res) {
+    db.movies.findOne({ _id: req.params.id }, function (err, result) {
+        if(err){
+            res.json(500, err);
+            return;
+        }
+        if(!result){
+            res.json(404, {erro: "We did not find a movie with id " +req.params.id });
+            return;
+        }
+        res.json(200, result);
+    });
+});
 
-    switch (body.action) {
-        case "getMovies":
-            db.movies.find({}, respond);
-            break;
-
-        case "addMovie":
-            db.movies.insert({title: body.title}, respond);
-            break;
-
-        case "rateMovie":
-            db.movies.update({title: body.title}, { $set: {rating: body.rating}}, function (err, num) {
-                respond(err, {success: num +" records updated"});
-            });
-            break;
-
-        default:
-            respond("No action given");
-    }
-
+app.put('/movies/:id', function (req, res) {
+    db.movies.update({ _id: req.params.id }, req.body, { upsert: false }, function (err, num, upsert) {
+        if (err) {
+            res.json(500, { error: err });
+            return;
+        }
+        if (num === 0) {
+            res.json(400, { error: { message: "No records were updated." }});
+            return;
+        }
+        //res.send(204);
+        res.json(200, { success: { message: "Sucessfully updated movie with ID " + req.params.id }});
+    });
+});
+app.delete('/movies/:id', function (req, res) {
+    db.movies.remove({ _id: req.params.id }, function (err, num) {
+        if(err){
+            res.json(500, err);
+            return;
+        }
+        if(num === 0){
+            res.json(404, {erro: "We did not find a movie with id " +req.params.id });
+            return;
+        }
+        res.set('Link', root+'/movies; rel="collection"');
+        res.send(204);
+    });
 })
-.listen(3000);
+
+app.listen(port);
