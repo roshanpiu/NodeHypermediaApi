ffe0d6a991eb9a65e23691200ca9be344c91bcd9
Roshan Piumal <ropiumal@gmail.com>
2016-03-19 18:08:44 +0530

    set up of the basic app

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var express = require('express');
 var app = express();
 var Datastore = require('nedb');
 var db = {};
+
+db.movies = new Datastore({filename: 'db/movies', autoload: true});
+
+app.get('/',function (req, res) {
+    res.end("The API is working");
+}).listen(3000);
