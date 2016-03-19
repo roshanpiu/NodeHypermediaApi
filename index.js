5df87748110b9a7bdc51789655ef962b1ba0231d
Roshan Piumal <ropiumal@gmail.com>
2016-03-19 18:46:52 +0530

    implemented the api using the rpc style

--- index.js
+++ index.js
@@ -5,6 +5,42 @@ var db = {};
 
 db.movies = new Datastore({filename: 'db/movies', autoload: true});
 
+app.use(express.bodyParser());
+
 app.get('/',function (req, res) {
     res.end("The API is working");
-}).listen(3000);
+})
+.post('/rpc', function (req, res) {
+    var body = req.body;
+
+    var respond = function (err, results) {
+        if(err){
+            res.send(JSON.stringify(err));
+        }else {
+            res.send(JSON.stringify(results));
+        }
+    }
+
+    res.set('Content-type', 'application/json')
+
+    switch (body.action) {
+        case "getMovies":
+            db.movies.find({}, respond);
+            break;
+
+        case "addMovie":
+            db.movies.insert({title: body.title}, respond);
+            break;
+
+        case "rateMovie":
+            db.movies.update({title: body.title}, { $set: {rating: body.rating}}, function (err, num) {
+                respond(err, {success: num +" records updated"});
+            });
+            break;
+
+        default:
+            respond("No action given");
+    }
+
+})
+.listen(3000);
